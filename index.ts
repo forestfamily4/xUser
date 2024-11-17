f8bdbfd6c80f15edfa6c19e6fdc805236414b0d2
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2024-11-18 01:03:47 +0900

    a

--- index.ts
+++ index.ts
@@ -4,9 +4,10 @@ import { Twitter } from "./lib/twitter/twitter";
 async function main() {
   process.on("uncaughtException", (err)=>console.log(err));
   const server=new Server("http://localhost:3000")
-  const client=new Twitter()
-  await client.login()
-  client.startTimer()  
+  server.start()
+  // const client=new Twitter()
+  // await client.login()
+  // client.startTimer()  
 } 
 
 main()
--- lib/server.ts
+++ lib/server.ts
@@ -14,9 +14,14 @@ export class Server {
       res.send("ok");
     });
 
-    this.app.listen(3000);
     this.uptime();
   }
+  public start(){    
+    this.app.listen(3000).on("listening", () => {
+      console.log("Server is running on port 3000");
+    })
+  }
+
   private uptime() {
     setTimeout(async () => {
       const res = await axios.get(this.host, {
