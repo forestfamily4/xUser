644883ed35167f25b7138f31fcad19cb7f038670
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2024-11-18 01:12:36 +0900

    delete uptime

--- lib/server.ts
+++ lib/server.ts
@@ -8,28 +8,13 @@ export class Server {
     this.app = express();
     this.now = new Date().getTime();
     this.app.get("/", (req, res) => {
-      if (req.query.key === this.now.toString()) {
-        this.uptime();
-      }
       res.send("ok");
     });
-
-    this.uptime();
   }
-  public start(){   
-    const port=process.env.PORT || 4000 
+  public start() {
+    const port = process.env.PORT || 4000
     this.app.listen(port).on("listening", () => {
       console.log(`server started on ${port}`);
     })
-  }
-
-  private uptime() {
-    setTimeout(async () => {
-      const res = await axios.get(this.host, {
-        params: {
-          key: this.now,
-        },
-      });
-    }, 30 * 1000);
   }
 }
