96afe358b19fb81d5b8aa6c28b7f29bf2806c5a9
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2024-11-26 09:43:30 +0900

    a

--- lib/twitter/twitter.ts
+++ lib/twitter/twitter.ts
@@ -48,7 +48,7 @@ export class Twitter {
       const tweets = (await this.client?.getTweetApi().getHomeTimeline({ "count": 20 }))?.data.data
       if (!tweets) { return; }
       for await (const i of tweets.keys()) {
-        if(i>2){break}
+        if(i<18){continue}
         const tweet = tweets[i]
         const text = tweet?.tweet.legacy?.fullText
         console.log(text)
